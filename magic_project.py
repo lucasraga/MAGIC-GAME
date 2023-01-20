c729c4bfa7d4249f0220ab0f2a9395883fe253c7
lucasraga <94230216+lucasraga@users.noreply.github.com>
2023-01-20 17:24:04 +0100

    Ideas

--- magic_project.py
+++ magic_project.py
@@ -2,4 +2,12 @@
 Propiedad intelectual de David y Lucas.
 Todos los derechos reservados.
 '''
-
+#Abrimos y leemos la base de datos
+magicbd = open("D:\LUCAS\DAW\Programación\python\proyectos\magic-game/BD_magic_cartas.txt")
+mensaje = magicbd.read()
+#Printeamos la base de datos y posteriormente la cerramos.
+print(mensaje)
+magicbd.close()
+'''
+La idea es utilizarlo para pedirle al usuario que elija la carta que desea utilizar contra otra. Podemos empezar con eso he ir avanzando desde ahí.
+'''
