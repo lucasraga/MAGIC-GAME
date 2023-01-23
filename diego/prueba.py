58e788ee435680fd2311411a6548fe7f6eb11b0d
SantDevelop <123370659+SantDevelop@users.noreply.github.com>
2023-01-23 12:40:07 +0100

    Update prueba.py

--- diego/prueba.py
+++ diego/prueba.py
@@ -7,7 +7,7 @@ ventana.title("Ventana prueba Arcane Signet")
 ventana.geometry("500x500")
 
 # Cargar la imagen
-imagen = PhotoImage(file="C:\Users\dgill\OneDrive\Documentos\Personal\magic-python\diego\img\arcane-signet.png")
+imagen = PhotoImage(file="")
 
 # Crear una etiqueta para mostrar la imagen
 etiqueta_imagen = tk.Label(ventana, image=imagen)
